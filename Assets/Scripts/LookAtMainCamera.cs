b7ad3c9a946a0a31830e341c65b49e8d5c99d2ec
Shinyou Park <shinyouwith@gmail.com>
2020-06-01 20:58:43 +0900

    [chamomile] 프로젝트 asset 추가

---
+++ Assets/Scripts/LookAtMainCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtMainCamera : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        RotateSpriteTowardsCamera();
+    }
+
+    void RotateSpriteTowardsCamera()
+    {
+        Vector3 targetVector = Camera.main.transform.position - transform.position;
+
+        float newYAngle = Mathf.Atan2(targetVector.z, targetVector.x) * Mathf.Rad2Deg - 90;
+
+        transform.rotation = Quaternion.Euler(0, -1 * newYAngle, 0);
+    }
+}
---
+++ Assets/Scripts/PlayerTouchManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class PlayerTouchManager : MonoBehaviour
+{
+    private VideoPlayer videoPlayer;
+
+    private void Start()
+    {
+        GameObject videoPlane = GameObject.Find("SheepVideo");
+        videoPlayer = videoPlane.GetComponent<VideoPlayer>();
+    }
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            toggleVideoPlayerState();
+        }
+    }
+
+    private void toggleVideoPlayerState() {
+        if (videoPlayer.isPlaying)
+        {
+            videoPlayer.Pause();
+        }
+        else {
+            videoPlayer.Play();
+        }
+    }
+}
